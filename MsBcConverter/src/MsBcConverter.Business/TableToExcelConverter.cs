04b51b4cd9200e5224f139907ff3fdce80d900c9
Rudolf Dittrich <rudi.dittrich77@gmail.com>
2022-12-21 15:48:08 +0100

    Refactore TableToExcelConverter

--- MsBcConverter/src/MsBcConverter.Business/TableToExcelConverter.cs
+++ MsBcConverter/src/MsBcConverter.Business/TableToExcelConverter.cs
@@ -1,6 +1,5 @@
 ﻿using FastExcel;
 using MsBcConverter.Interface;
-using System.Collections;
 
 namespace MsBcConverter.Business
 {
@@ -10,39 +9,52 @@ namespace MsBcConverter.Business
         {
             using (FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(new FileInfo(excelTemplatePath), false))
             {
-                var worksheet = fastExcel.Worksheets.SingleOrDefault(w => w.Name == "Artikel");
+                var worksheet = GetWorksheet(fastExcel, "Artikel");
 
-                if (worksheet == null)
-                {
-                    throw new ArgumentNullException(nameof(worksheet));
-                }
+                InserRows(dataTable, worksheet);
 
-                worksheet.Read();
-
-                var rows = new List<Row>();
-                rows.AddRange(worksheet.Rows);
-
-                var rowNumber = rows.Count + 1;
-
-                foreach (var row in dataTable.Rows)
-                {
-                    if (row.Count == 0) continue;
-
-                    var cells = new List<Cell>();
-                    for (int columnNumber = 1; columnNumber <= dataTable.Header.Count; columnNumber++)
-                    {
-                        cells.Add(new Cell(columnNumber, row[columnNumber - 1]));
-                    }
-
-                    rows.Add(new Row(rowNumber, cells));
-                    ++rowNumber;
-                }
-
-                worksheet.Rows = rows;
                 fastExcel.Write(worksheet);
             }
 
             return excelTemplatePath;
+        }
+
+        private static Worksheet GetWorksheet(FastExcel.FastExcel fastExcel, string worksheetName)
+        {
+            var worksheet = fastExcel.Worksheets.SingleOrDefault(w => w.Name == worksheetName);
+
+            if (worksheet == null)
+            {
+                throw new ArgumentNullException(nameof(worksheet));
+            }
+
+            worksheet.Read();
+
+            return worksheet;
+        }
+
+        private static void InserRows(DataTable dataTable, Worksheet worksheet)
+        {
+            var rows = new List<Row>();
+            rows.AddRange(worksheet.Rows);
+
+            var rowNumber = rows.Count + 1;
+
+            foreach (var row in dataTable.Rows)
+            {
+                if (row.Count == 0) continue;
+
+                var cells = new List<Cell>();
+                for (int columnNumber = 1; columnNumber <= dataTable.Header.Count; columnNumber++)
+                {
+                    cells.Add(new Cell(columnNumber, row[columnNumber - 1]));
+                }
+
+                rows.Add(new Row(rowNumber, cells));
+                ++rowNumber;
+            }
+
+            worksheet.Rows = rows;
         }
     }
 }
--- MsBcConverter/src/MsBcConverter.Tests/TableToExcelConverterTests.cs
+++ MsBcConverter/src/MsBcConverter.Tests/TableToExcelConverterTests.cs
@@ -23,6 +23,52 @@ namespace MsBcConverter.Tests
             Assert.IsTrue(true);
         }
 
+        // Playground
+        //[TestMethod]
+        //public void Read_Excel_File()
+        //{
+        //    var excelFile = "Templates/import-artikel.xlsx";
+        //    var excelFile3 = "Templates/import-artikel-3.xlsx";
+        //    DataTable dataTable = ReadData();
+
+        //    using (FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(new FileInfo(excelFile), new FileInfo(excelFile3)))
+        //    {
+        //        var worksheet = fastExcel.Worksheets.SingleOrDefault(w => w.Name == "Artikel");
+        //        worksheet.Read();
+
+
+        //        var rows = new List<Row>();
+        //        rows.AddRange(worksheet.Rows);
+
+        //        var rowNumber = rows.Count + 1;
+
+        //        foreach (var row in dataTable.Rows)
+        //        {
+        //            if (row.Count == 0) continue;
+
+        //            var cells = new List<Cell>();
+        //            for (int columnNumber = 1; columnNumber <= dataTable.Header.Count; columnNumber++)
+        //            {
+        //                cells.Add(new Cell(columnNumber, row[columnNumber - 1]));
+        //            }
+
+        //            rows.Add(new Row(rowNumber, cells));
+        //            ++rowNumber;
+
+        //        }
+
+        //        worksheet.Rows = rows;
+        //        fastExcel.Write(worksheet, "Artikel");
+
+        //        Debug.WriteLine(string.Format("Worksheet Name:{0}, Index:{1}", worksheet.Name, worksheet.Index));
+
+        //        //To read the rows call read
+        //        worksheet.Read();
+        //        //Do something with rows
+        //        Debug.WriteLine(string.Format("Worksheet Rows:{0}", rows.Count()));
+        //    }
+        //}
+
         private static DataTable ReadData()
         {
             var articles = File.ReadAllText("Resources/gemappte_artikel.txt");
@@ -35,7 +81,6 @@ namespace MsBcConverter.Tests
                 Header = header,
                 Rows = rows
             };
-
             return dataTable;
         }
     }
